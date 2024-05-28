bc9b23d7364299a92485887fc455b4a13fde2087
aaaroz <ram.ardiansyah18@gmail.com>
2024-05-28 22:24:29 +0700

    refactor : change meta and link icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
+import { VitePWA } from "vite-plugin-pwa";
+import { seoConfig, manifest } from "./src/helpers/seo-config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: seoConfig.baseURL,
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
+    compress(),
   ],
   image: {
     remotePatterns: [
@@ -21,4 +28,19 @@ export default defineConfig({
   },
   output: "server",
   adapter: vercel(),
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: manifest,
+        workbox: {
+          globDirectory: "dist",
+          globPatterns: [
+            "**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+          ],
+          navigateFallback: null,
+        },
+      }),
+    ],
+  },
 });
