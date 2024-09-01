e8bb5ef41a15b12c5d4e20acfb129e14df996b6e
Muhamad Rhamdhan Mardiansyah <136713175+aaaroz@users.noreply.github.com>
2024-09-01 16:42:13 +0700

    Update download-resume.tsx

--- src/components/elements/download-resume.tsx
+++ src/components/elements/download-resume.tsx
@@ -4,7 +4,7 @@ import { BiSolidDownvote } from "react-icons/bi";
 export const DownloadResume = (): React.ReactElement => {
   return (
     <a
-      href="https://drive.google.com/file/d/1VjIZkMgMgKYg-5jE6IqxUpP_0_jmg0j2/view?usp=sharing"
+      href="https://drive.google.com/file/d/1H77zHEZCcWkPzF9o-O-3b8FzfW0Mvkm8/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer"
       className="group flex items-center gap-2 text-muted-foreground font-semibold transition-all duration-300 hover:text-muted-foreground/80 hover:underline"
